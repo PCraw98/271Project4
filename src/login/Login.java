28f0861909ec9541df405c4f14eef376936cf631
Parker Crawford <parkercrawford@u.boisestate.edu>
2018-09-09 20:47:05 -0600

    Committing Login

--- src/login/Login.java
+++ src/login/Login.java
@@ -1,5 +1,27 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.BorderLayout;
+
 package login;
 
-public class Login {
-
+public class Login extends JFrame {
+	
+	JFrame frame = new JFrame("Login Screen");
+	//exits JFrame operations when the window is closed
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setPreferredSize(new Dimension(250,150));
+	frame.pack();
+	frame.setVisible(True);
+	/**
+	 * 
+	 */
+	private JPanel mainPanel() {
+		//initializes a new panel object called panel
+		JPanel panel = new JPanel();
+		panel.setLayout(Grid);
+		//use JPasswordField to use dots instead of text
+		
+	}
 }
